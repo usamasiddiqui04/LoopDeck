1739905a3658d9073b56d4ceca8e6a32f697f579
Usama Ali <usamashiekh33@gmail.com>
2021-04-05 17:23:55 +0500

    Replace Edited File Done

--- app/src/main/java/com/example/loopdeck/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/loopdeck/ui/main/MainActivity.kt
@@ -26,62 +26,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun initViews() {
 
-        btn_gallery.setOnClickListener {
-//
-//            val HTTP_TRANSPORT = com.google.api.client.http.javanet.NetHttpTransport()
-//
-//
-//            val service: Drive =
-//                Drive.Builder(
-//                    HTTP_TRANSPORT,
-//                    DriveQuickstart.JSON_FACTORY,
-//                    DriveQuickstart.getCredentials(this, HTTP_TRANSPORT)
-//                )
-//                    .setApplicationName(DriveQuickstart.APPLICATION_NAME)
-//                    .build()
-//
-//
-//            // Print the names and IDs for up to 10 files.
-//
-//            // Print the names and IDs for up to 10 files.
-//            val result: FileList = service.files().list()
-//                .setPageSize(100)
-//                .setFields("nextPageToken, files(id, name)")
-//                .execute()
-//            val files: List<File>? = result.files
-//            if (files == null || files.isEmpty()) {
-//                println("No files found.")
-//            } else {
-//                println("Files:")
-//                for (file in files) {
-//
-//                    //TODO: Get thumbnail link here and assign it to imageView
-//                    file.thumbnailLink
-////                    println("${file.getName()} ${file.getId()} ${file.fileExtension}")
-//                    println(file.toString())
-//
-//                }
-//            }
-
-            startActivity(
-                Intent(
-                    this@MainActivity,
-                    GoogleDriveActivity::class.java
-                )
-            )
-
-//            startActivity(Intent(this@MainActivity, MainActivity::class.java))
-        }
-
-        btn_video.setOnClickListener {
-            startActivity(
-                Intent(
-                    this@MainActivity,
-                    com.obs.marveleditor.MainActivity::class.java
-                )
-            )
-        }
-
         btn_recents.setOnClickListener {
             startActivity(
                 Intent(
