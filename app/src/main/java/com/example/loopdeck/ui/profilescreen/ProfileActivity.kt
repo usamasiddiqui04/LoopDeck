ef99ea47c8c8fbdeb53eb5982938fad12240d635
Usama Ali <usamashiekh33@gmail.com>
2021-05-04 06:32:08 +0500

    complete ProfileActivity.kt

--- app/src/main/java/com/example/loopdeck/ui/profilescreen/ProfileActivity.kt
+++ app/src/main/java/com/example/loopdeck/ui/profilescreen/ProfileActivity.kt
@@ -1,12 +1,24 @@
 package com.example.loopdeck.ui.profilescreen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.loopdeck.R
+import com.example.loopdeck.ui.changepassword.ChangePasswordActivity
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.activity_profile.toolbar
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        toolbar.setNavigationIcon(R.drawable.ic_back_black)
+        toolbar.setNavigationOnClickListener { onBackPressed() }
+
+        changepasswordlayout.setOnClickListener {
+            val intent = Intent(this, ChangePasswordActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
